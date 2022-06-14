39aba6f5c769c41d7f97a09705ba63139a12cca7
JacopoNa <j.nardelli@virgilio.it>
2022-06-14 15:02:20 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// CAMPO MINATO
+
+// utente sceglie (con un prompt) il livello di difficoltà in base al range di numeri possibili di gioco:
+    // - (1) tra 1 e 100, -(2) tra 1 e 81, (3) tra 1 e 49
+// computer genera 16 numeri random (bombe) nel range prescelto che non possono essere duplicati
+// finchè il gioco non è finito:
+    // utente inserisce un numero alla volta (prompt)
+        // se il numero inserito è presente nella lista delle "bombe" --> gioco finito --> messaggio 
+        // altrimenti il gioco va avanti finchè si raggiunge il massimo numero di tentativi possibili --> gioco finito --> messaggio
+// al termine della partita sofware comunica numero di volte che l'utente ha scelto un numero che non era nelle bombe (punteggio)
